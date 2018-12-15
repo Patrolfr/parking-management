01e06b9b97914ab6be3e69dec1d6a51e24aecd12
Patrolfr <patrolwiking@gmail.com>
2018-12-15 15:39:52 +0100

    initial commit

---
+++ src/main/java/komo/fraczek/toukparking/ToukparkingApplication.java
@@ -0,0 +1,14 @@
+package komo.fraczek.toukparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToukparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToukparkingApplication.class, args);
+	}
+
+}
+
