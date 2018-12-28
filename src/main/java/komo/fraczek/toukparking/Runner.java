c860408f0f94c48c55fffddcf1a563b278fc7d09
Patrolfr <patrolwiking@gmail.com>
2018-12-28 16:09:05 +0100

    start.sh; travis.yml;

--- src/main/java/komo/fraczek/toukparking/Runner.java
+++ src/main/java/komo/fraczek/toukparking/Runner.java
@@ -1,24 +1,11 @@
 package komo.fraczek.toukparking;
 
-import komo.fraczek.toukparking.charge.ChargeCalculator;
-import komo.fraczek.toukparking.charge.Math;
-import komo.fraczek.toukparking.domain.DriverType;
-import komo.fraczek.toukparking.domain.ParkingMeter;
-import komo.fraczek.toukparking.domain.ParkingStatus;
 import komo.fraczek.toukparking.service.BillRepository;
 import komo.fraczek.toukparking.service.MeterRepository;
 import komo.fraczek.toukparking.service.ParkingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import static org.apache.commons.lang3.time.DateUtils.round;
 
 
 @Component
@@ -36,24 +23,8 @@ public class Runner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-
-        LocalDate localDate = LocalDate.of(2018, 12, 15);
-
-        BigDecimal v = parkingService.calculateDailyIncome(localDate);
-
-        List<ParkingMeter> all = meterRepository.findAll();
-
-        all.forEach(System.out::println);
-
-        System.out.println(v);
-
-        System.out.println(billRepository.findByNumberPlateAndParkingStatus("WEQ2123", ParkingStatus.FINISHED).get().toString());
-
-//        System.out.println("********");
-//        IntStream.range(2,7).mapToDouble(val -> Math.countGeomSeriesTerm.apply((double) val, 1.2)).forEach(System.out::println);
-//        System.out.println("********");
-//        IntStream.range(1,7).mapToDouble(val -> Math.calculateGeometricSeriesSum(val, 1.2)).forEach(System.out::println);
-//        System.out.println("********");
+//        List<ParkingMeter> all = meterRepository.findAll();
+//        all.forEach(System.out::println);
 
     }
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,3 @@
-
-
 -- -- --Bills
 -- insert into parking_bill(id, PARKING_FEE, PARKING_TIME_IN_HOURS) values (1, 2.2, 5);
 insert into parking_bill values (1, '2018-12-15', 'REGULAR', 'WEQ2123', 2.22, 'FINISHED', 1 );
@@ -13,17 +11,23 @@ insert into parking_bill values (6, '2018-12-20', 'REGULAR', 'MGQ2124', 5.55, 'F
 insert into parking_bill values (7, null, 'REGULAR', 'ASD2123', 0, 'OCCUPIED', 0);
 insert into parking_bill values (8, null, 'DISABLED', 'MGQ2124', 0, 'OCCUPIED', 0);
 
+//        System.out.println("********");
+//        IntStream.range(2,7).mapToDouble(val -> Math.countGeomSeriesTerm.apply((double) val, 1.2)).forEach(System.out::println);
+//        System.out.println("********");
+//        IntStream.range(1,7).mapToDouble(val -> Math.calculateGeometricSeriesSum(val, 1.2)).forEach(System.out::println);
+//        System.out.println("********");
+
 --finished parkings
---  12-05
- insert into parking_meter values (1, 'KJS-417', '2018-12-15 13:20:00', '2018-12-05 15:46:06', 1);
- insert into parking_meter values (2, 'ZXC-417', '2018-12-15 13:20:00', '2018-12-05 16:46:06', 2);
- insert into parking_meter values (3, 'KJS-417', '2018-12-15 13:20:00', '2018-12-05 17:46:06', 3);
- insert into parking_meter values (4, 'KJS-417', '2018-12-15 13:20:00', '2018-12-05 18:46:06', 4);
+--  12-15
+insert into parking_meter values (1, 'KJS-417', '2018-12-15 13:20:00', '2018-12-05 15:46:06', 1);
+insert into parking_meter values (2, 'ZXC-417', '2018-12-15 13:20:00', '2018-12-05 16:46:06', 2);
+insert into parking_meter values (3, 'KJS-417', '2018-12-15 13:20:00', '2018-12-05 17:46:06', 3);
+insert into parking_meter values (4, 'KJS-417', '2018-12-15 13:20:00', '2018-12-05 18:46:06', 4);
 -- -- 12-20
- insert into parking_meter values (5, 'KJS-417', '2018-12-20 13:20:00', '2018-12-20 17:46:06', 5);
- insert into parking_meter values (6, 'KJS-417', '2018-12-20 13:20:00', '2018-12-20 18:46:06', 6);
+insert into parking_meter values (5, 'KJS-417', '2018-12-20 13:20:00', '2018-12-20 17:46:06', 5);
+insert into parking_meter values (6, 'KJS-417', '2018-12-20 13:20:00', '2018-12-20 18:46:06', 6);
 -- -- -- --Occupied parkings
- insert into parking_meter values (7, 'TST-123', '2018-12-26 13:20:00', null, 7);
- insert into parking_meter values (8, 'STS-123', '2018-12-26 13:20:00', null, 8);
+insert into parking_meter values (7, 'TST-123', '2018-12-26 13:20:00', null, 7);
+insert into parking_meter values (8, 'STS-123', '2018-12-26 13:20:00', null, 8);
 
 
