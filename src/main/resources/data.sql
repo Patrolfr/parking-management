945b011c87c2a1621919c6c78633a45eb5102e79
Patrolfr <patrolwiking@gmail.com>
2018-12-30 17:16:51 +0100

    data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,7 @@
  insert into parking_meter values (3, 'REGULAR', 'WAW7544Y', 'KJS-417', 10, 'FINISHED', '2018-12-05 13:20:00', '2018-12-05', '14:46:06');
  insert into parking_meter values (4, 'DISABLED', 'GST2717X', 'XGA-817', 10, 'FINISHED', '2018-12-05 16:56:00', '2018-12-05', '21:03:12');
  insert into parking_meter values (5, 'REGULAR', 'GAR6201Z', 'OQW-379', 10, 'FINISHED', '2018-12-05 11:20:00', '2018-12-05', '15:26:41');
- insert into parking_meter values (6, 'DISABLED', 'LAB1821B', 'LRS-921', 10, 'FINISHED', '2018-12-05 017:14:30', '2018-12-06', '11:46:45');
+ insert into parking_meter values (6, 'DISABLED', 'LAB1821B', 'LRS-921', 10git, 'FINISHED', '2018-12-05 017:14:30', '2018-12-06', '11:46:45');
 --
 --  -- 12-10
  insert into parking_meter values (7, 'REGULAR', 'AZC7544Y', 'FCS-417', 0, 'FINISHED', '2018-12-10 13:20:00', '2018-12-10', '14:23:37');
